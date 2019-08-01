e036832bdbc65abca317e42ab4f548ddcab57a7b
alan hernandez <indexcomplex@gmail.com>
2019-08-01 15:08:46 -0700

    Settings App save to user defaults

--- Settings Saved/SettingsVC.swift
+++ Settings Saved/SettingsVC.swift
@@ -10,81 +10,108 @@ import UIKit
 
 class SettingsVC: UITableViewController {
 
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var likesDevSwitch: UISwitch!
+    @IBOutlet weak var coolValueMeterValue: UILabel!
+    @IBOutlet weak var coolMeterSlider: UISlider!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+       loadSettings()
+    
     }
 
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    
+    
+    // we set te sender to UISLider so we expect that and only that
+    @IBAction func didSlideValueMeter(_ sender: UISlider) {
+        
+        let wholeNumber = Int(sender.value)
+        coolValueMeterValue.text = String(wholeNumber)
+        
+        
+        
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    func loadSettings(){
+        // key must be exct to the saved
+//       let firstName =   UserDefaults.standard.string(forKey: "firstName")
+//        print(firstName ?? "No first name saved")
+        
+        guard
+            let settingsDictionary = UserDefaults.standard.dictionary(forKey: "settings"),
+            let firstName = settingsDictionary["firstName"] as? String,
+            let lastName = settingsDictionary["lastNae"] as? String,
+            let likesDev = settingsDictionary["likesDev"] as? Bool,
+            let coolMeterValue = settingsDictionary["coolMeterValue"] as? Int
+            else {return}
+        
+        firstNameTextField.text = firstName
+        lastNameTextField.text = lastName
+        likesDevSwitch.isOn = likesDev
+        coolValueMeterValue.text = String(coolMeterValue)
+        coolMeterSlider.value = Float(coolMeterValue)
+        
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    
+    
+    
+    
+    func saveSettings(){
+        
+        let settingsDictionary : [String: Any?] = ["firstName": firstNameTextField.text, "lastName:": lastNameTextField.text, "likesDev:" :likesDevSwitch.isOn, "coolMeterValue:": Int(coolMeterSlider.value)]
+        
+        //to Save its easy
+//         UserDefaults.standard.set(firstNameTextField.text, forKey: "firstName")
+//        UserDefaults.standard.set(lastNameTextField.text, forKey: "LastName")
+        
+        
+        UserDefaults.standard.set(settingsDictionary, forKey: "settings")
+        
+        
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func didSaveAlert() {
+        
+        let alert = UIAlertController(title: "Savd Settigs", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel)
+        alert.addAction(action)
+        present(alert,animated:  true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    
+    //tableVC has function didSelectRow
+    // we have to override as its already predefinded so OverRide bc we're gonna redefine it
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == 2 else{return}
+        print("Save")
+        saveSettings()
+        DispatchQueue.main.async {
+            self.didSaveAlert()
+        }
+        
+        
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
+    
+    
 }
